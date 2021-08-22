476a6c82f75f131abae85ba2b91420fdc7f852af
mmianowany <mateuszmianovany@gmail.com>
2021-08-22 18:58:29 +0200

    BPM control without slider refresh

--- lib/utils/metro_audio.dart
+++ lib/utils/metro_audio.dart
@@ -1,15 +1,29 @@
+import 'dart:async';
+
 import 'package:audioplayers/audioplayers.dart';
+import 'package:async/async.dart';
 
 class MetroAudio {
-  AudioPlayer instance = new AudioPlayer();
-  AudioCache cache = new AudioCache();
+  AudioPlayer _instance = new AudioPlayer();
+  AudioCache _cache = new AudioCache();
+  late Timer _tickTimer;
+  late int _tickIntervalBPS;
 
-  Future<AudioPlayer> play(int bpm) async {
-    instance = await cache.loop("Metronome2.wav");
-    return instance;
+
+  void play(int bpm) {
+    _tickIntervalBPS = (60 /bpm * 1000).toInt();
+    _tickTimer = new Timer.periodic(new Duration(milliseconds: _tickIntervalBPS), _onTick);
+
+  }
+
+  Future<AudioPlayer> _onTick(Timer t) async {
+    _instance = await _cache.play("Metronome2.wav");
+    return _instance;
+
   }
 
   void stop() async {
-    instance.pause();
+    _tickTimer.cancel();
+    _instance.pause();
   }
 }
