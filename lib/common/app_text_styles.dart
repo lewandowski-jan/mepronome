bd9fe96fb323bdba9d34d8228f69f2daa26ccbdb
Jan Lewandowski <jan.lewandowski@leancode.pl>
2021-08-22 21:22:22 +0200

    Add text styles file, extract home page, refactor

---
+++ lib/common/app_text_styles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+class AppTextStyles {
+  static const medium = TextStyle(
+    fontFamily: 'Metropolis',
+    color: AppColors.PrimaryAccent,
+    fontSize: 20,
+  );
+  static const h1 = TextStyle(
+    fontFamily: 'Metropolis',
+    color: AppColors.PrimaryLight,
+    fontSize: 60,
+  );
+}
