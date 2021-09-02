4899e07ce57da47accc066c9c9defccd4e1b2981
Jan Lewandowski <jan.lewandowski@leancode.pl>
2021-09-02 18:35:47 +0200

    Change to quiver, change to soundpool

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -7,6 +7,7 @@
 import 'package:audio_session/audio_session_web.dart';
 import 'package:audioplayers/web/audioplayers_web.dart';
 import 'package:just_audio_web/just_audio_web.dart';
+import 'package:soundpool_web/soundpool_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -15,5 +16,6 @@ void registerPlugins(Registrar registrar) {
   AudioSessionWeb.registerWith(registrar);
   AudioplayersPlugin.registerWith(registrar);
   JustAudioPlugin.registerWith(registrar);
+  SoundpoolPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:metronome/home_page.dart';
+import 'package:metronome/utils/metro_audio.dart';
 
+final metro = MetroAudio();
 void main() async {
-  runApp(MyApp());
+  runApp(MetronomeApp());
 }
 
-class MyApp extends StatelessWidget {
+class MetronomeApp extends StatefulWidget {
+  MetronomeApp({
+    Key? key,
+  }) : super(
+          key: key,
+        );
+
+  @override
+  _MetronomeAppState createState() => _MetronomeAppState();
+}
+
+class _MetronomeAppState extends State<MetronomeApp> {
+  final metroAudio = MetroAudio();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'mepronome',
-      home: HomePage(),
+      home: HomePage(
+        metroAudio: metroAudio,
+      ),
     );
+  }
+
+  @override
+  dispose() {
+    this.metroAudio.dispose();
+    super.dispose();
   }
 }
