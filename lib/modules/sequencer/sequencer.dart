cbdcd04f060a977cbbdd15b8b68268663a889bb7
Michał Kowalik <michal.t.kowalik@gmail.com>
2021-11-26 21:49:18 +0100

    List of sequences created, changing parameters of the sequence available

--- lib/modules/sequencer/sequencer.dart
+++ lib/modules/sequencer/sequencer.dart
@@ -1,61 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:metronome/common/app_colors.dart';
+import 'package:metronome/hamburger_menu.dart';
 import 'package:metronome/modules/sequencer/widgets/add_sequence.dart';
 import 'package:metronome/modules/sequencer/widgets/sequence.dart';
 
-class Sequencer extends StatelessWidget {
-  const Sequencer({Key? key}) : super(key: key);
+class Sequencer extends HookWidget {
+  Sequencer({Key? key}) : super(key: key);
+
+  final _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
+    final sequencesList = useState(<Widget>[]);
+    final sequenceIndex = useState(0);
+
     return Scaffold(
+      drawer: HamburgerMenu(),
       backgroundColor: AppColors.PrimaryLight,
-      body: SafeArea(
-        bottom: false,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Flexible(
-                flex: 1,
-                child: Container(
-                  margin: const EdgeInsets.fromLTRB(0, 24, 0, 16),
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(
-                        'assets/mepronome_logo.png',
+      body: Stack(
+        children: <Widget>[
+          SafeArea(
+          bottom: false,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: Container(
+                    margin: const EdgeInsets.fromLTRB(0, 24, 0, 16),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'assets/mepronome_logo.png',
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              Flexible(
-                flex: 5,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(50),
+                Flexible(
+                  flex: 5,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(50),
+                      ),
+                      gradient: AppColors.gradientPrimaryDarkGradientDark,
                     ),
-                    gradient: AppColors.gradientPrimaryDarkGradientDark,
+                    child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 50, width: double.infinity),
+                      Wrap(
+                        alignment: WrapAlignment.start,
+                        children: [...sequencesList.value,AddSequence(onClick: (){
+                          if(sequencesList.value.length<9) {
+                            sequencesList.value = [...sequencesList.value,
+                              Sequence(index: sequenceIndex.value)];
+                            sequenceIndex.value++;
+                          }
+                        },)],
+                      ),
+                    ],
                   ),
-                  child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 50, width: double.infinity),
-                    Wrap(
-                      alignment: WrapAlignment.start,
-                      children: [
-                        Sequence(),
-                        AddSequence(),
-                      ],
-                    ),
-                  ],
+                  ),
                 ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
+        new Positioned(
+          top: 0.0,
+          left: 2.0,
+          right: 0.0,
+          child: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.menu, size: 33, color: Colors.black,), // change this size and style
+              onPressed: () => _scaffoldKey.currentState!.openDrawer(),
+            ),
+            backgroundColor: Colors.transparent,
+            elevation: 0.0, //Shadow gone
+          ),
+        ),
+        ],
       ),
     );
   }
